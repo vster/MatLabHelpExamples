d71aadfb6675c83ac7d0d2d39f6807162683d164
Vital Teresh <vster2006@gmail.com>
2018-02-26 13:06:33 +0300

    add adjoint example

---
+++ Symbolic Math Toolbox/adjoint_1.m
@@ -0,0 +1,15 @@
+% adjoint
+
+syms x y z
+A = sym([x y z; 2 1 0; 1 0 2]);
+X = adjoint(A)
+
+isAlways(A*X == det(A)*eye(3))
+
+isAlways(det(A)*eye(3) == X*A)
+
+syms a b c d
+A = [a b; c d];
+invA = adjoint(A)/det(A)
+
+isAlways(invA == inv(A))
